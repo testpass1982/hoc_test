6634680f99adc97d45238e129f0a87b70e9f7078
anatoly popov <popov.anatoly@gmail.com>
2019-05-18 17:39:33 +0300

    add some code

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    title = models.CharField(u'Test name', max_length=20)
+    test_options = models.BooleanField(u'Правильный ответ', default=False)
+
+    class Meta:
+        verbose_name="Test"
+        verbose_name_plural="Tests"
+
+    def __str__(self):
+        return self.title
